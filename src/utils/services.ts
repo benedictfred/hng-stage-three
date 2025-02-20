e407b2714f8437c8a477463e0ba497d284cae0f2
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-02-21 00:17:08 +0100

    fix: api errors

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -4,11 +4,11 @@ export const translateLanguage = async (
   text: string
 ) => {
   try {
-    const translator = await self.ai.translator.create({
+    const translator = await self?.ai?.translator?.create({
       sourceLanguage: srcLng,
       targetLanguage: targetLng,
     });
-    const translatedText = await translator.translate(text);
+    const translatedText = await translator?.translate(text);
     if (!translatedText) throw new Error("Couldn't translate. Try again");
     return translatedText;
   } catch (error) {
@@ -21,30 +21,30 @@ export const translateLanguage = async (
 export const detectLanguage = async (text: string) => {
   try {
     const languageDetectorCapabilities =
-      await self.ai.languageDetector.capabilities();
-    const canDetect = languageDetectorCapabilities.capabilities;
+      await self?.ai?.languageDetector?.capabilities();
+    const canDetect = languageDetectorCapabilities?.capabilities;
     let detector;
     if (canDetect === "no") {
       return;
     }
     if (canDetect === "readily") {
-      detector = await self.ai.languageDetector.create();
+      detector = await self?.ai?.languageDetector?.create();
     } else {
-      detector = await self.ai.languageDetector.create({
+      detector = await self?.ai?.languageDetector?.create({
         monitor(m: any) {
           m.addEventListener("downloadprogress", (e: ProgressEvent) => {
             console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
           });
         },
       });
-      await detector.ready;
+      await detector?.ready;
     }
 
-    const results = await detector.detect(text);
+    const results = await detector?.detect(text);
 
-    if (results.length > 0) {
+    if (results?.length > 0) {
       const topResult = results[0];
-      return topResult.detectedLanguage;
+      return topResult?.detectedLanguage;
     } else {
       throw new Error("No language detected.");
     }
